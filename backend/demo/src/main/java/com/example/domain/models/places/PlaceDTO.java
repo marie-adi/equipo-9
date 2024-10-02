6499217a1e76923953c02f7cc2e4e929e10c02cd
sanesloEduca <sandra.estebanlopez@educa.madrid.org>
2024-10-02 17:17:29 +0200

    add DTOS

---
+++ backend/demo/src/main/java/com/example/domain/models/places/PlaceDTO.java
@@ -0,0 +1,20 @@
+package com.example.domain.models.places;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlaceDTO {
+    private Long id;
+    private String name;
+    private String city;
+    private String address;
+    private String problem;
+    private Integer ranking;
+    private String imageUrl;
+}
