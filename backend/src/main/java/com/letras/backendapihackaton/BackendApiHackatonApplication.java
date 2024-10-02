e22d27db3337f3bf35d54d3bc701cc5a7d55a191
sanesloEduca <sandra.estebanlopez@educa.madrid.org>
2024-10-02 14:07:39 +0200

    backend

---
+++ backend/src/main/java/com/letras/backendapihackaton/BackendApiHackatonApplication.java
@@ -0,0 +1,14 @@
+package com.letras.backendapihackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiHackatonApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendApiHackatonApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/letras/backendapihackaton/BackendApiHackatonApplicationTests.java
@@ -0,0 +1,14 @@
+package com.letras.backendapihackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiHackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
