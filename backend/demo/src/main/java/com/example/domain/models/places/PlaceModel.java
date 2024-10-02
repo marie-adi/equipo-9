f06bcb09052b836f88229cf0baebee2053e80675
sanesloEduca <sandra.estebanlopez@educa.madrid.org>
2024-10-02 16:17:00 +0200

    add models

---
+++ backend/demo/src/main/java/com/example/domain/models/places/PlaceModel.java
@@ -0,0 +1,35 @@
+package com.example.domain.models.places;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name="places")
+
+public class PlaceModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String city;
+    private String address;
+    private String problem;
+    private Integer ranking;
+    private String imageUrl;
+}
